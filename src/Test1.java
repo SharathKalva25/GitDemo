95bb4e58331d635df2e70b003c8dd8bb645daf3a
Sharath Kalva <Sharath Kalva@LAPTOP-IUECR3BE>
2021-12-16 12:15:35 +0530

    First Commit

---
+++ src/Test1.java
@@ -0,0 +1,16 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String check="GIT";
+		
+		if(check.equals("GIT"))
+			System.out.println("True");
+		else
+			System.out.println("False");
+		
+	}
+
+}
